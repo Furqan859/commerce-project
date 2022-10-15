824e5a916c674261071aa1e0de1d005d72dacd8c
Furqan-Rasool <furqanrasool2@gmail.com>
2022-10-15 05:54:03 +0500

    comple e-comm website in react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useThemeHook} from './GlobalComponent/ThemeProvider';
+import Header from './component/Header';
+import {Router} from '@reach/router';
+
+//pages
+
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
+  const [theme] = useThemeHook();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={theme ? 'bg-black' : 'bg-light-2'} style={{height:'100vh', overflowY:'auto'}} >
+     <Header/>
+     <Router>
+     <Home path="/"/>
+     <Cart path="/cart"/>
+     </Router>
+    </main>
   );
 }
 
